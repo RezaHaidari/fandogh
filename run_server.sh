2eaa94f0b34b8622eb8d1a8d045751019ffc9f40
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2018-04-29 09:47:27 +0200

    build fandogh nginx docker

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,6 @@
 set -x 
 NETWORK_NAME='fandogh-temp'
+NGINX_NAME='fandogh-nginx'
 NETWORK_EXISTS=`docker network ls -f name="^$NETWORK_NAME$"| wc -l  | xargs echo`
 if [ $NETWORK_EXISTS -eq 1 ];
 then
@@ -8,4 +9,10 @@ then
 else
   echo "$NETWORK_NAME already exists"
 fi
+
+docker run -d  --name $NGINX_NAME \
+  --network $NETWORK_NAME \
+  --publish 8080:80 \
+  -v `pwd`/dockers/nginx/conf.d:/etc/nginx/conf.d \
+  nginx:fandogh
 
