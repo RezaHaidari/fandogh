ac9544016583472d8fc3b2c3d0a6276363f179ec
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2018-04-30 23:15:02 +0200

    implement image builder task

---
+++ server/fandogh/image/image_builder.py
@@ -0,0 +1,61 @@
+from concurrent.futures import ThreadPoolExecutor
+
+import docker
+import zipfile
+
+from docker.errors import BuildError, APIError
+
+from image.models import Build
+
+executor = ThreadPoolExecutor(4)
+client = docker.from_env()
+
+
+def trigger_image_building(version):
+    executor.submit(build_task, version)
+
+
+def build_task(version):
+    img_build = Build(version=version)
+    img_build.save()
+    version.state = 'BUILDING'
+    version.save()
+    workspace = prepare_workspace(version)
+    img_build.logs = 'workspace is ready.\n'
+    img_build.save()
+    (img, log) = build(version.app.name, version.version, workspace)
+    img_build.logs += log
+    img_build.save()
+    if img is None:
+        version.state = 'FAILED'
+    else:
+        version.state = 'BUILT'
+    version.save()
+
+
+def prepare_workspace(version):
+    zip_ref = zipfile.ZipFile(version.source.open())
+    zip_ref.extractall('/tmp/workspace/')
+    zip_ref.close()
+
+    # TODO: naive implementation
+    return '/tmp/workspace/'
+
+
+def build(app_name, version, workspace):
+    tag = ":".join([app_name, version])
+    img = None
+    try:
+        img, output_stream = client.images.build(path=workspace, tag=tag)
+        log_result = ''.join([chunk['stream'] for chunk in output_stream if 'stream' in chunk])
+        print(log_result)
+    except BuildError as e:
+        log_result = e.build_log
+    except APIError as e:
+        log_result = str(e)
+    except ConnectionError as e:
+        log_result = str(e)
+    except Exception as e:
+        log_result = str(e)
+
+    return img, log_result
--- server/fandogh/image/models.py
+++ server/fandogh/image/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 from webapp.models import AppVersion
+import uuid
 
 
 class Build(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     logs = models.TextField()
     version = models.ForeignKey(AppVersion, on_delete=models.DO_NOTHING, related_name='builds')
     start_date = models.DateTimeField(auto_now_add=True)
