654603c8b602cba99a6e5266e2e373f5f8b495ea
soroosh sarabadani <soroosh.sarabadani@klarna.com>
2018-04-30 18:11:38 +0200

    implement prepare_workspace

---
+++ server/fandogh/image/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Build(models.Model):
+    # logs
+    # build-id
+    # relevent app version
+    # start date
+    # state -> or in version table
+    # logs
+    pass
