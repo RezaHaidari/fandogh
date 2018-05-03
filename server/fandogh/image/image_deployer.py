15fb7531e81b918ba3689e3dd35508d44d51b533
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2018-05-03 21:10:41 +0200

    add cpu & mem limit to containers

--- server/fandogh/image/image_deployer.py
+++ server/fandogh/image/image_deployer.py
@@ -14,7 +14,7 @@ def deploy(app, version):
     running_containers = client.containers.list(all=True, filters={'name': name})
     if running_containers:
         running_containers[0].remove(force=True)
-    c = client.containers.run(img_name, detach=True, name=name, network=network)
+    c = client.containers.run(img_name, detach=True, name=name, network=network, mem_limit='100m', cpu_quota=10)
     container = Container(container_id=c.id, name=c.name)
     container.save()
     return container
