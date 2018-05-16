f3fce1ec2f2f1727871b3ca452efb7d4079a3056
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2018-05-16 23:39:44 +0200

    fix image urls

--- server/fandogh/image/urls.py
+++ server/fandogh/image/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^/(?P<image_name>.+)/versions/(?P<image_version>.+)/builds', ImageBuildView.as_view()),
-    url(r'^/(?P<image_name>.+)/versions', ImageVersionView.as_view()),
-    url(r'^/', ImageView.as_view()),
+    url(r'^(?P<image_name>.+)/versions/(?P<image_version>.+)/builds', ImageBuildView.as_view()),
+    url(r'^(?P<image_name>.+)/versions', ImageVersionView.as_view()),
+    url(r'^', ImageView.as_view()),
 ]
