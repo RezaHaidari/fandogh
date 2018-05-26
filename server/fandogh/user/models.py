3cb8e67c5f53e7810a9b8cc02aae9cb9432294f5
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2018-05-26 19:11:36 +0200

    add namespace model

--- server/fandogh/user/models.py
+++ server/fandogh/user/models.py
@@ -1,6 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
 class EarlyAccessRequest(models.Model):
     email = models.EmailField(unique=True)
+
+
+class Namespace(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.OneToOneField(to=User, related_name='namespace', on_delete=models.CASCADE)
