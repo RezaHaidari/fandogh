ef839643ede1698e5d8c83dec4c4ef86d3da42fa
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2018-06-15 14:50:13 +0200

    add DefaultMysqlServiceDeployer

--- server/fandogh/service/managed_services/mysql.py
+++ server/fandogh/service/managed_services/mysql.py
@@ -7,3 +7,20 @@ mysql_stack = DeploymentStack([
 })
 
 
+class ManagedServiceDeployer(object):
+    def deploy(self, variate_name):
+        pass
+
+
+class DefaultMysqlServiceDeployer(ManagedServiceDeployer):
+    def deploy(self, variate_name, context):
+        # TODO: validate context
+        mysql_stack.deploy(context)
+
+
+mysql_deployer = DefaultMysqlServiceDeployer()
+deployer = {'mysql': mysql_deployer}
+
+
+def get_deployer(managed_service_name):
+    return deployer.get(managed_service_name, None)
