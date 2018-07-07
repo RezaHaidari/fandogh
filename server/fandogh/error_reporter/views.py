152202e4fa6a4cd75cf2ba9057be87467a960c8a
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2018-07-07 21:45:51 +0200

    return OK response

--- server/fandogh/error_reporter/views.py
+++ server/fandogh/error_reporter/views.py
@@ -1,10 +1,14 @@
 import logging
 
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
+
 cli_error_logger = logging.getLogger("cli_error")
+OKResponse = Response("OK")
 
 
 class ErrorsView(APIView):
     def post(self, request):
         cli_error_logger.info(request.data)
+        return OKResponse
