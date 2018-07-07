19ea77f37a1f072b9550c4742170d188260d9456
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2018-07-07 20:37:03 +0200

    handle authentation error

--- server/fandogh/api/exception_handler.py
+++ server/fandogh/api/exception_handler.py
@@ -1,8 +1,9 @@
 import logging
-from django.utils.translation import ugettext_lazy
-from jwt import ExpiredSignatureError
-from rest_framework import status
 import traceback
+
+from django.utils.translation import ugettext_lazy
+from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
 
 from common.response import GeneralResponse
 
@@ -24,8 +25,8 @@ META: {}
 """.strip().format(request.method, request.path, type(exc), str(exc), traceback.format_exc(), request.GET, request.POST,
                    request.user.id, request.META))
 
-    if type(exc) is ExpiredSignatureError:
-        return GeneralResponse("Your token is expired. Please login again", status=status.HTTP_401_UNAUTHORIZED)
+    if type(exc) is AuthenticationFailed:
+        return GeneralResponse("Your token is not valid. Please login first", status=status.HTTP_401_UNAUTHORIZED)
 
     return GeneralResponse(
         ugettext_lazy("Sorry about this inconvenience, there is a problem in our side, we'll fix it soon"),
